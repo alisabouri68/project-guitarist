6244cf4907668cdf5c150daee96422a12afdca72
alisabouri <alisabouri6857@gmail.com>
2024-11-22 21:59:28 +0330

    end project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,16 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    fontFamily : {
-      body : ["PermanentMarker-Regular"]
-    },
+    // fontFamily : {
+    //   body : ["PermanentMarker-Regular"]
+    // },
     extend: {
       backgroundImage: {
         "header-img" : "url('../img/hero-section-min.jpg')",
         "section-fixe" : "url('../img/about-me-2-min-1024x683.jpg')",
         "guitar-lessons" : "url('../img/guitar-lessons.jpg')",
         "guitar-stay-in-tuch" : "url('../img/stay-in-touch.jpg')",
+        "footer-profile" : "url('../img/photo_2024-11-09_00-15-04.jpg')",
       },
 
      },
